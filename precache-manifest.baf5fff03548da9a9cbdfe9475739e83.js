e61ed1c6bae4db42b392d20e5529f0cb5c4c0a9f
makindemayowa <mankinde53@yahoo.com>
2019-02-18 00:12:41 +0100

    Updates

---
+++ precache-manifest.baf5fff03548da9a9cbdfe9475739e83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ee1f122685a216acebe",
+    "url": "/randomNoGenerator/static/css/main.a1b4de12.chunk.css"
+  },
+  {
+    "revision": "4ee1f122685a216acebe",
+    "url": "/randomNoGenerator/static/js/main.4ee1f122.chunk.js"
+  },
+  {
+    "revision": "169ca2cf6cfab3be2bcb",
+    "url": "/randomNoGenerator/static/css/1.41509197.chunk.css"
+  },
+  {
+    "revision": "169ca2cf6cfab3be2bcb",
+    "url": "/randomNoGenerator/static/js/1.169ca2cf.chunk.js"
+  },
+  {
+    "revision": "6fa2e183e5d1e8463426",
+    "url": "/randomNoGenerator/static/js/runtime~main.6fa2e183.js"
+  },
+  {
+    "revision": "137cf6231ce72fbc0718f71b6cd1fd27",
+    "url": "/randomNoGenerator/index.html"
+  }
+];
